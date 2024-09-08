d0727041c7b661ef16df25c12684503d8390f709
Farzaneh Zamanian <152534852+farzaneh-zamanian@users.noreply.github.com>
2024-09-08 21:02:57 +0430

    routing defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/templates/Home/HomePage";
+import NotFoundPage from "./components/templates/404/NotFoundPage";
+import Layout from "./Layouts/Layout";
 function App() {
-
   return (
-    <></>
- 
-  )
+    <div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Layout>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import styles from "./Layout.module.css"
+import React, { Children } from "react";
+import styles from "./Layout.module.css";
 
-function Layout() {
+function Layout({ children }) {
   return (
-    <div>Layout</div>
-  )
+    <>
+      <header className={styles.header}>
+        <h1>Crypto App</h1>
+        <p>React js Project</p>
+      </header>
+      {children}
+      <footer className={styles.footer}>
+        <p>Developed By ZamanianZ</p>
+      </footer>
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
